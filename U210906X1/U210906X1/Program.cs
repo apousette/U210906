4370acc2ad33d9e2d94a304d80105e916f508337
apousette <adam021@live.se>
2021-09-07 13:05:02 +0200

    inmatning, sen orduppdelning och sist utskrift av orden + mängden karaktärer

--- U210906X1/U210906X1/Program.cs
+++ U210906X1/U210906X1/Program.cs
@@ -6,7 +6,17 @@ namespace U210906X1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Skriv en mening:");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split();
+            Console.WriteLine(words.Length);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word} ({words.Length})");
+            }
+
         }
     }
 }
